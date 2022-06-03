ded2ced2b584bffad24691936699037a3bce4354
Stanislaw <s.szarapka10@gmail.com>
2022-06-03 12:24:06 +0200

    fetch data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import API_KEYS from './api-keys'
+
 import CurrentPrice from './source/components/CurrentPrice'
 import Value from './source/components/Value'
 import CalculateButton from './source/components/CalculateButton'
@@ -10,6 +12,38 @@ import SettingsModal from './source/components/SettingsModal'
 import Footer from './source/components/Footer'
 
 const App = () => {
+    let URL = ''
+    const fetchCurrencyData = async (
+        type = 'all',
+        from = 'PLN',
+        to = 'PLN',
+        amount = 1
+    ) => {
+        const headers = new Headers()
+        headers.append('apikey', API_KEYS)
+
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers,
+        }
+
+        if (type === 'all')
+            URL = 'https://api.apilayer.com/exchangerates_data/symbols'
+        else if (type === 'convert')
+            URL = `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`
+
+        let data = {}
+        await fetch(URL, requestOptions)
+            .then(response => response.json())
+            .then(result => (data = result))
+            .catch(error => console.log('error', error))
+
+        return data
+    }
+
+    fetchCurrencyData('all')
+
     return (
         <StrictMode>
             <main>
