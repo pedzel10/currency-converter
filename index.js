c08bacca200085c7af7903bda638c1df06a2cae5
Stanislaw <s.szarapka10@gmail.com>
2022-06-06 16:46:02 +0200

    add Context for decimal and thousands separators

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode, useEffect, useState } from 'react'
+import React, { StrictMode, createContext, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import API_KEYS from './api-keys'
@@ -11,7 +11,19 @@ import SettingsButton from './source/components/SettingsButton'
 import SettingsModal from './source/components/SettingsModal'
 import Footer from './source/components/Footer'
 
+export const SeparatorsContext = createContext({
+    decimal: ',',
+    thousands: '.',
+    formatNumber: function () {},
+})
+
 const App = () => {
+    const [separators, setSeparators] = useState({
+        decimal: ',',
+        thousands: '.',
+        formatNumber,
+    })
+
     const [currencyList, setcurrencyList] = useState(null)
 
     const [inputValue, setInputValue] = useState(1)
@@ -20,6 +32,10 @@ const App = () => {
     const [resultCurrency, setResultCurrency] = useState('PLN')
 
     const [price, setPrice] = useState(1)
+
+    const formatNumber = number => {
+        return number
+    }
 
     const showResult = result => {
         const resultV = result.result
@@ -65,6 +81,11 @@ const App = () => {
 
     useEffect(() => {
         fetchCurrencyData('all')
+        setSeparators({
+            decimal: ',',
+            thousands: '.',
+            formatNumber,
+        })
     }, [])
 
     // za pierwszym razem inputValue się nie zmienia
@@ -80,52 +101,60 @@ const App = () => {
     return (
         <StrictMode>
             <main>
-                <CurrentPrice
-                    inputValue={inputValue}
-                    inputCurrency={inputCurrency}
-                    resultValue={resultValue}
-                    resultCurrency={resultCurrency}
-                    price={price}
-                />
-                <Value
-                    data={currencyList}
-                    amount={inputValue}
-                    currency={inputCurrency}
-                    setInputValue={setInputValue}
-                    setResultValue={setResultValue}
-                    setCurrency={setInputCurrency}
-                    type="input"
-                />
-                <Value
-                    data={currencyList}
-                    amount={resultValue}
-                    currency={resultCurrency}
-                    setInputValue={setInputValue}
-                    setResultValue={setResultValue}
-                    setCurrency={setResultCurrency}
-                    type="result"
-                />
-                <CalculateButton
-                    inputValue={inputValue}
-                    setInputValue={setInputValue}
-                    inputCurrency={inputCurrency}
-                    resultValue={resultValue}
-                    setResultValue={setResultValue}
-                    resultCurrency={resultCurrency}
-                    fetchCurrencyData={fetchCurrencyData}
-                />
-                <SwapButton
-                    inputValue={inputValue}
-                    setInputValue={setInputValue}
-                    inputCurrency={inputCurrency}
-                    setInputCurrency={setInputCurrency}
-                    resultValue={resultValue}
-                    setResultValue={setResultValue}
-                    resultCurrency={resultCurrency}
-                    setResultCurrency={setResultCurrency}
-                />
-                <SettingsButton />
-                <SettingsModal />
+                <SeparatorsContext.Provider
+                    value={{
+                        decimal: separators.decimal,
+                        thousands: separators.thousands,
+                        formatNumber: separators.formatNumber,
+                    }}
+                >
+                    <CurrentPrice
+                        inputValue={inputValue}
+                        inputCurrency={inputCurrency}
+                        resultValue={resultValue}
+                        resultCurrency={resultCurrency}
+                        price={price}
+                    />
+                    <Value
+                        data={currencyList}
+                        amount={inputValue}
+                        currency={inputCurrency}
+                        setInputValue={setInputValue}
+                        setResultValue={setResultValue}
+                        setCurrency={setInputCurrency}
+                        type="input"
+                    />
+                    <Value
+                        data={currencyList}
+                        amount={resultValue}
+                        currency={resultCurrency}
+                        setInputValue={setInputValue}
+                        setResultValue={setResultValue}
+                        setCurrency={setResultCurrency}
+                        type="result"
+                    />
+                    <CalculateButton
+                        inputValue={inputValue}
+                        setInputValue={setInputValue}
+                        inputCurrency={inputCurrency}
+                        resultValue={resultValue}
+                        setResultValue={setResultValue}
+                        resultCurrency={resultCurrency}
+                        fetchCurrencyData={fetchCurrencyData}
+                    />
+                    <SwapButton
+                        inputValue={inputValue}
+                        setInputValue={setInputValue}
+                        inputCurrency={inputCurrency}
+                        setInputCurrency={setInputCurrency}
+                        resultValue={resultValue}
+                        setResultValue={setResultValue}
+                        resultCurrency={resultCurrency}
+                        setResultCurrency={setResultCurrency}
+                    />
+                    <SettingsButton />
+                    <SettingsModal />
+                </SeparatorsContext.Provider>
                 <Footer />
             </main>
         </StrictMode>
