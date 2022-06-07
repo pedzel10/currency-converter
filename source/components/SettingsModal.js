7cf1c496a301dfcf767aa8a509d935234dca41e3
Stanislaw <s.szarapka10@gmail.com>
2022-06-07 10:16:14 +0200

    add seprarators change settings

--- source/components/SettingsModal.js
+++ source/components/SettingsModal.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
-const SettingsModal = () => {
+import { SeparatorsContext } from '/index.js'
+
+const SettingsModal = ({ setSeparators }) => {
+    // const { decimal, thousands, formatNumber } = useContext(SeparatorsContext)
+    // console.log(decimal, thousands, formatNumber)
+
+    const setDotAsDecimalSeparator = () => {
+        setSeparators(prevState => {
+            return {
+                ...prevState,
+                decimal: '.',
+                thousands: ',',
+                formatNumber: prevState.formatNumber,
+            }
+        })
+    }
+    const setComaAsDecimalSeparator = () => {
+        setSeparators(prevState => {
+            return {
+                ...prevState,
+                decimal: ',',
+                thousands: '.',
+                formatNumber: prevState.formatNumber,
+            }
+        })
+    }
+
     return (
         <>
             <div>
-                <label>Separator tysięcy</label>
-                <button>.</button>
-                <button>,</button>
+                <label>Separator dziesiętny</label>
+                <button onClick={setDotAsDecimalSeparator}>.</button>
+                <button onClick={setComaAsDecimalSeparator}>,</button>
             </div>
             <div>
                 <label>Separator tysięcy</label>
-                <button>.</button>
-                <button>,</button>
+                <button onClick={setComaAsDecimalSeparator}>.</button>
+                <button onClick={setDotAsDecimalSeparator}>,</button>
             </div>
         </>
     )
